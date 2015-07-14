86fc110394639a836e65dabc83de7a8dae46494d
Karan Chopra <k4chopra@uwaterloo.ca>
2015-07-14 13:37:57 -0400

    Method for selecting device
    All the other methods should call this and this should return the ID of
    the device (right now it just prints the ID)

---
+++ crabselect.sh
@@ -0,0 +1,46 @@
+
+numDevices=$(($(adb devices | wc -l) - 2))
+devices=$(adb devices)
+# crabSelect() {
+	deviceList=""
+
+	if [[ "$numDevices" == "0" ]]; then
+		{
+			echo "No devices detected"
+		}
+ 	#This needs to be modified, right now it just prints the ID
+	elif [[ "$numDevices" == "1" ]]; then
+		{
+		echo $devices ":"
+		}	
+	else
+		{
+		#Modified part of superInstall
+		echo "Select a device using its number:"
+		for ((i = 2; i <= (($numDevices) +1); i++))
+		do
+			deviceID=$(adb devices | tail -n +$i | head -n1 | cut -f 1 | xargs -I X)
+			deviceMake=$(adb -s $deviceID shell getprop ro.product.manufacturer | tr -d '\r')
+			deviceName=$(adb -s $deviceID shell getprop ro.product.model | tr -d '\r')
+			deviceOS=$(adb -s $deviceID shell getprop ro.build.version.release | tr -d '\r')
+			
+			#Adds each device to the string		
+			deviceList="$deviceList \"${deviceMake}  -  ${deviceName}  -  ${deviceID}  -  ${deviceOS}\""
+			
+		done
+					
+		echo $deviceList
+		
+		#Asks user to select device, and then prints out the device.
+		eval set $deviceList
+		select device in "$@"; 
+		do
+			#We need to return this value to the method that called it or save it to a global variable or something, but for now I just printed it
+			echo $device | cut -f 5 -d " ";
+			break
+		done;
+		}
+	fi
+# }
+
+
